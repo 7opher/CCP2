c775bb13c4d4d9654e324cca85b3b1ea9bbc1ffa
christopher <chris7opher@outlook.fr>
2020-09-01 09:17:37 +0200

    file that checks that the fields are full

---
+++ admin/if_field_verification.php
@@ -0,0 +1,22 @@
+<?php
+if(empty($name)) 
+        {
+            $nameError = 'Ce champ ne peut pas être vide';
+            $isSuccess = false;
+        }
+        if(empty($description)) 
+        {
+            $descriptionError = 'Ce champ ne peut pas être vide';
+            $isSuccess = false;
+        } 
+        if(empty($price)) 
+        {
+            $priceError = 'Ce champ ne peut pas être vide';
+            $isSuccess = false;
+        } 
+        if(empty($category)) 
+        {
+            $categoryError = 'Ce champ ne peut pas être vide';
+            $isSuccess = false;
+        }
+?>
